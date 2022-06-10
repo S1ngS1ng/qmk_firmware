a7c95e91aa6525c110482bc1cc10692343392b75
S1ngS1ng <liuxing0514@gmail.com>
2022-06-10 10:00:58 -0700

    add dynamic macro

---
+++ keyboards/idobao/id75/keymaps/s1ngs1ng/config.h
@@ -0,0 +1,2 @@
+// Do not allow one macro to contain the other macro
+#define DYNAMIC_MACRO_NO_NESTING
--- keyboards/idobao/id75/keymaps/s1ngs1ng/rules.mk
+++ keyboards/idobao/id75/keymaps/s1ngs1ng/rules.mk
@@ -1,3 +1,5 @@
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+VIA_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
