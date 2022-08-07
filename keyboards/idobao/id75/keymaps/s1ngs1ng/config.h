5c73602796d921b6214ca5ebc5a7cb51d0d2ac47
S1ngS1ng <liuxing0514@gmail.com>
2022-08-07 15:26:43 -0700

    fix compile issue, update keymap

--- keyboards/idobao/id75/keymaps/s1ngs1ng/config.h
+++ keyboards/idobao/id75/keymaps/s1ngs1ng/config.h
@@ -1,2 +1,3 @@
 // Do not allow one macro to contain the other macro
 #define DYNAMIC_MACRO_NO_NESTING
+#define RGBLED_NUM 75
--- keyboards/idobao/id75/keymaps/s1ngs1ng/keymap.c
+++ keyboards/idobao/id75/keymaps/s1ngs1ng/keymap.c
@@ -29,23 +29,23 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* QWERTY
    * .--------------------------------------------------------------------------------------------------------------------------------------.
-   * | `/~    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | BSPACE | RGB RM | RGB MO | RGB TG |
+   * | `/~    | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | BSPACE | BSPACE | RGB RMO| RGB TG |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-   * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      |   \    | 7      | 8      | 9      |
+   * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      |   \    |   \    | RGB MO |   \    |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-   * | CTLESC | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | ENTER  | 4      | 5      | 6      |
+   * | CTLESC | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | ENTER  | ENTER  | 5      | PGUP   |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-   * | LSHIFT | Z      | X      | C      | V      | B      | N      | M      | ,      | .      | /      | RSHIFT | 1      | 2      | 3      |
+   * | LSHIFT | Z      | X      | C      | V      | B      | N      | M      | ,      | .      | /      | RSHIFT | RSHIFT | UP     | PGDOWN |
    * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-   * | LCTRL  | LCTRL  | LALT   | LGUI   | LWR(1) | SPACE  | SPACE  | RSE(2) | LEFT   | DOWN   | UP     | RIGHT  | PGDOWN | 0      | PGUP   |
+   * | LCTRL  | LCTRL  | LALT   | LGUI   | LWR(1) | SPACE  | SPACE  | RSE(2) | LEFT   | DOWN   | UP     | RIGHT  | LEFT   | DOWN   | RIGHT  |
    * '--------------------------------------------------------------------------------------------------------------------------------------'
    */
   [QWERTY] = LAYOUT_ortho_5x15(
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, RGB_RMOD,RGB_MOD, RGB_TOG,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_7,    KC_8,    KC_9,
-      CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_4,    KC_5,    KC_6,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_1,    KC_2,    KC_3,
-      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_PGDN, KC_0,    KC_PGUP
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, KC_BSPC, RGB_RMOD,RGB_TOG,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_BSLS, RGB_MOD, KC_BSLS,
+      CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  KC_ENT,  KC_5,    KC_PGUP,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN,
+      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* LOWER (symbol & Fn)
